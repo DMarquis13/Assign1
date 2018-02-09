e63bf37b7e5c5cf6c0a932a5ad5be7dba8c5a360
Unknown <renegade0130@gmail.com>
2018-02-09 00:08:10 -0600

    Adding JSON objects files

---
+++ jsonlab1.js
@@ -0,0 +1,268 @@
+var tanks = {{
+    "name":"Char d'Assault Schneider",
+    "service_year":1916,
+    "national_origin":"France",
+    "total_production":400,
+    "crew":7,
+    "weight":14,
+    "top_speed":5,
+    "max_range":50,
+    
+},
+
+{
+    "name":"Char d'Assault St. Chamond
+    ",
+    "service_year":1916,
+    "national_origin":"France",
+    "total_production":377,
+    "crew":8,
+    "weight":25,
+    "top_speed":7,
+    "max_range":37,
+    
+},
+
+{
+    "name":"FIAT 2000
+"    ,
+    "service_year":1918,
+    "national_origin":"Italy",
+    "total_production":2,
+    "crew":10,
+    "weight":40,
+    "top_speed":4,
+    "max_range":47,
+    
+},
+
+{
+    "name":"Ford Model 1918 3-ton",
+    "service_year":1918,
+    "national_origin":"United States",
+    "total_production":15,
+    "crew":2,
+    "weight":3,
+    "top_speed":8,
+    "max_range":34,
+    
+},
+
+{
+    "name":"K Grosskampfwagen",
+    "service_year":1918,
+    "national_origin":"Germany",
+    "total_production":2,
+    "crew":22,
+    "weight":120,
+    "top_speed":5,
+    "max_range":,
+    
+},
+
+{
+    "name":"Leichter Kampfwagen I",
+    "service_year":1918,
+    "national_origin":"Germany",
+    "total_production":2,
+    "crew":3,
+    "weight":7,
+    "top_speed":10,
+    "max_range":43,
+    
+},
+
+{
+    "name":"Leichter Kampfwagen II",
+    "service_year":1918,
+    "national_origin":"Germany",
+    "total_production":2,
+    "crew":3,
+    "weight":9,
+    "top_speed":10,
+    "max_range":43,
+    
+},
+
+{
+    "name":"Little Willie Landship",
+    "service_year":1915,
+    "national_origin":"United Kingdom",
+    "total_production":1,
+    "crew":4,
+    "weight":20,
+    "top_speed":2,
+    "max_range":,
+    
+},
+
+{
+    "name": "M1917 6-ton",
+    "service_year": 1918,
+    "national_origin": "United States",
+    "total_production": 950,
+    "crew": 2,
+    "weight": 7,
+    "top_speed": 5,
+    "max_range": 30
+  },
+  {
+    "name": "Medium Mark C (Hornet)",
+    "service_year": 1918,
+    "national_origin": "United Kingdom",
+    "total_production": 50,
+    "crew": 4,
+    "weight": 22,
+    "top_speed": 8,
+    "max_range": 75
+  },
+  {
+    "name": "Medium Tank Mk A (Whippet)",
+    "service_year": 1918,
+    "national_origin": "United Kingdom",
+    "total_production": 200,
+    "crew": 3,
+    "weight": 14,
+    "top_speed": 8,
+    "max_range": 40
+  },
+  {
+    "name": "Model 1917 Holt Gas-Electric",
+    "service_year": 1918,
+    "national_origin": "United States",
+    "total_production": 1,
+    "crew": 6,
+    "weight": 28,
+    "top_speed": 6,
+    "max_range": 31
+  },
+  {
+    "name": "Renault FT-17",
+    "service_year": 1917,
+    "national_origin": "France",
+    "total_production": 3694,
+    "crew": 2,
+    "weight": 7,
+    "top_speed": 5,
+    "max_range": 22
+  },
+  {
+    "name": "Skeleton Tank",
+    "service_year": 1918,
+    "national_origin": "United States",
+    "total_production": 1,
+    "crew": 2,
+    "weight": 9,
+    "top_speed": 5,
+    "max_range": ""
+  },
+  {
+    "name": "Sturmpanzerwagen A7V",
+    "service_year": 1917,
+    "national_origin": "Germany",
+    "total_production": 20,
+    "crew": 18,
+    "weight": 33,
+    "top_speed": 8,
+    "max_range": 25
+  },
+  {
+    "name": "Sturmpanzerwagen A7V-U",
+    "service_year": 1918,
+    "national_origin": "Germany",
+    "total_production": 0,
+    "crew": 8,
+    "weight": 28,
+    "top_speed": 4,
+    "max_range": 34
+  },
+  {
+    "name": "Sturmpanzerwagen Oberschlesien",
+    "service_year": 1918,
+    "national_origin": "Germany",
+    "total_production": 0,
+    "crew": 5,
+    "weight": 19,
+    "top_speed": 6,
+    "max_range": 37
+  },
+  {
+    "name": "Tank Mark IX",
+    "service_year": 1918,
+    "national_origin": "United Kingdom",
+    "total_production": 34,
+    "crew": 4,
+    "weight": 30,
+    "top_speed": 3,
+    "max_range": 20
+  },
+  {
+    "name": "Tank Mark VIII (International / Liberty)",
+    "service_year": 1919,
+    "national_origin": "United States",
+    "total_production": 125,
+    "crew": 11,
+    "weight": 41,
+    "top_speed": 6,
+    "max_range": 50
+  },
+  {
+    "name": "Tank Mk I (Big Willie / Centipede / Mother)",
+    "service_year": 1916,
+    "national_origin": "United Kingdom",
+    "total_production": 150,
+    "crew": 8,
+    "weight": 31,
+    "top_speed": 4,
+    "max_range": 24
+  },
+  {
+    "name": "Tank Mk II",
+    "service_year": 1917,
+    "national_origin": "United Kingdom",
+    "total_production": 50,
+    "crew": 8,
+    "weight": 31,
+    "top_speed": 4,
+    "max_range": 24
+  },
+  {
+    "name": "Tank Mk IV",
+    "service_year": 1917,
+    "national_origin": "United Kingdom",
+    "total_production": 1220,
+    "crew": 8,
+    "weight": 28,
+    "top_speed": 4,
+    "max_range": 35
+  },
+  {
+    "name": "Tank Mk V",
+    "service_year": 1918,
+    "national_origin": "United Kingdom",
+    "total_production": 1004,
+    "crew": 8,
+    "weight": 29,
+    "top_speed": 5,
+    "max_range": 45
+  },
+  {
+    "name": "Tsar Tank",
+    "service_year": 1915,
+    "national_origin": "Russia",
+    "total_production": 1,
+    "crew": 18,
+    "weight": "",
+    "top_speed": "",
+    "max_range": ""
+  },
+  {
+    "name": "War Tank America",
+    "service_year": 1918,
+    "national_origin": "United States",
+    "total_production": 1,
+    "crew": 8,
+    "weight": 45,
+    "top_speed": 4,
+    "max_range": 37
+  }}
